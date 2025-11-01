cfd19bea7130913d52ba0e9731f5502484450a4c
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-11-01 16:43:20 -0500

    +tmp card list

--- src/lib/CARD_LIST.ts
+++ src/lib/CARD_LIST.ts
@@ -7,7 +7,28 @@ export type ICard = {
 	cardNumber: ICardNumber
 	pow: number
 	cost: number
-	langs: Record<string, {name: string; faction: string; ability: string; art: IUrl}>
+	langs: Record<
+		string,
+		{name: string; faction: keyof typeof FACTIONS; ability: string; art: IUrl}
+	>
 }
 
-export const CARD_LIST: Record<ICardNumber, ICard> = {}
+export const FACTIONS = ['PENGUIN', 'BEAR', 'SNOWMEN']
+
+/** @todo replace tmp card gen code */
+export const CARD_LIST: Record<ICardNumber, ICard> = Object.fromEntries(
+	Array(18)
+		.fill(1)
+		.map((startId, i) => {
+			const cardNumber = (startId + i).toString().padStart(3, '0')
+			return [
+				cardNumber,
+				{
+					cardNumber,
+					pow: 7,
+					cost: 7,
+					langs: {en: {name: 'Emperor Penguin', faction: 0, ability: '', art: ''}},
+				},
+			]
+		})
+)
