a65d4eb83981771dd8dac10801e17db51877f41e
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-11-01 16:11:21 -0500

    +card slot labels

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,12 @@ function App() {
 	const yourDeckSize = state.players[0].deck.length
 	const foeDeckSize = state.players[1].deck.length
 
+	const yourAttackers = state.players[0].attackers
+	const yourDefenders = state.players[0].defenders
+
+	const foeAttackers = state.players[1].attackers
+	const foeDefenders = state.players[1].defenders
+
 	return (
 		<>
 			<Todo />
@@ -19,51 +25,123 @@ function App() {
 					{foeDeckSize}
 				</div>
 
-				<div data-slot="foes-attacker-5"></div>
-				<div data-slot="foes-attacker-4"></div>
-				<div data-slot="foes-attacker-3"></div>
+				<div data-slot="foes-attacker-5" data-empty={!foeAttackers[5]}>
+					A
+				</div>
+				<div data-slot="foes-attacker-4" data-empty={!foeAttackers[4]}>
+					T
+				</div>
+				<div data-slot="foes-attacker-3" data-empty={!foeAttackers[3]}>
+					K
+				</div>
 
-				<div data-slot="foes-defender-5"></div>
-				<div data-slot="foes-defender-4"></div>
-				<div data-slot="foes-defender-3"></div>
+				<div data-slot="foes-defender-5" data-empty={!foeDefenders[5]}>
+					D
+				</div>
+				<div data-slot="foes-defender-4" data-empty={!foeDefenders[4]}>
+					E
+				</div>
+				<div data-slot="foes-defender-3" data-empty={!foeDefenders[3]}>
+					F
+				</div>
 
 				<div data-slot="empty"></div>
 				{/* new row */}
-				<div data-slot="foes-discard"></div>
+				<div
+					className="long-text"
+					data-slot="foes-discard"
+					data-stack={state.players[1].discard.length}
+				>
+					Discard
+				</div>
 
-				<div data-slot="foes-attacker-2"></div>
-				<div data-slot="foes-attacker-1"></div>
-				<div data-slot="foes-attacker-0"></div>
+				<div data-slot="foes-attacker-2" data-empty={!foeAttackers[2]}>
+					A
+				</div>
+				<div data-slot="foes-attacker-1" data-empty={!foeAttackers[1]}>
+					T
+				</div>
+				<div data-slot="foes-attacker-0" data-empty={!foeAttackers[0]}>
+					K
+				</div>
 
-				<div data-slot="foes-defender-2"></div>
-				<div data-slot="foes-defender-1"></div>
-				<div data-slot="foes-defender-0"></div>
+				<div data-slot="foes-defender-2" data-empty={!foeDefenders[2]}>
+					D
+				</div>
+				<div data-slot="foes-defender-1" data-empty={!foeDefenders[1]}>
+					E
+				</div>
+				<div data-slot="foes-defender-0" data-empty={!foeDefenders[0]}>
+					F
+				</div>
 
-				<div data-slot="foes-captives"></div>
+				<div
+					className="long-text"
+					data-slot="foes-captives"
+					data-stack={state.players[1].captives.length}
+				>
+					Captives
+				</div>
 
 				{/* dividing line */}
 
-				<div data-slot="your-captives"></div>
-				<div data-slot="your-defender-0"></div>
-				<div data-slot="your-defender-1"></div>
-				<div data-slot="your-defender-2"></div>
+				<div
+					className="long-text"
+					data-slot="your-captives"
+					data-stack={state.players[0].captives.length}
+				>
+					Captives
+				</div>
+				<div data-slot="your-defender-0" data-empty={!yourDefenders[0]}>
+					D
+				</div>
+				<div data-slot="your-defender-1" data-empty={!yourDefenders[1]}>
+					E
+				</div>
+				<div data-slot="your-defender-2" data-empty={!yourDefenders[2]}>
+					F
+				</div>
 
-				<div data-slot="your-attacker-0"></div>
-				<div data-slot="your-attacker-1"></div>
-				<div data-slot="your-attacker-2"></div>
+				<div data-slot="your-attacker-0" data-empty={!yourAttackers[0]}>
+					A
+				</div>
+				<div data-slot="your-attacker-1" data-empty={!yourAttackers[1]}>
+					T
+				</div>
+				<div data-slot="your-attacker-2" data-empty={!yourAttackers[2]}>
+					K
+				</div>
 
-				<div data-slot="your-discard"></div>
+				<div
+					className="long-text"
+					data-slot="your-discard"
+					data-stack={state.players[0].discard.length}
+				>
+					Discard
+				</div>
 
 				{/* new row */}
 				<div data-slot="empty"></div>
 
-				<div data-slot="your-defender-3"></div>
-				<div data-slot="your-defender-4"></div>
-				<div data-slot="your-defender-5"></div>
+				<div data-slot="your-defender-3" data-empty={!yourDefenders[3]}>
+					D
+				</div>
+				<div data-slot="your-defender-4" data-empty={!yourDefenders[4]}>
+					E
+				</div>
+				<div data-slot="your-defender-5" data-empty={!yourDefenders[5]}>
+					F
+				</div>
 
-				<div data-slot="your-attacker-3"></div>
-				<div data-slot="your-attacker-4"></div>
-				<div data-slot="your-attacker-5"></div>
+				<div data-slot="your-attacker-3" data-empty={!yourAttackers[3]}>
+					A
+				</div>
+				<div data-slot="your-attacker-4" data-empty={!yourAttackers[4]}>
+					T
+				</div>
+				<div data-slot="your-attacker-5" data-empty={!yourAttackers[5]}>
+					K
+				</div>
 
 				<div data-slot="your-deck" data-stack={yourDeckSize}>
 					{yourDeckSize}
