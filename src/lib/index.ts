ba2e7fd8ec8f02a505759135da8cca207c64a5c1
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-11-14 16:36:33 -0600

    +`drawFullHand()`
    This logic will be re-used 4 times during setup, so it has been separated out into its own file.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@ export * from './components'
 export * from './CARD_LIST'
 export * from './state'
 
+export * from './draw-hand'
 export * from './setup-attackers'
 export * from './shuffle'
--- src/lib/setup-attackers.ts
+++ src/lib/setup-attackers.ts
@@ -1,20 +1,10 @@
+import {drawFullHand} from './draw-hand'
 import type {IState} from './state'
 
 type IInt = number
 type IArg = {order: [[IInt, IInt, IInt], [IInt, IInt, IInt]]}
 
-export const setupAttackersDraw = (state: IState) => {
-	const [p1Hand, p1Deck] = splitArrayAt(3, state.players[0].deck)
-	const [p2Hand, p2Deck] = splitArrayAt(3, state.players[1].deck)
-
-	state.players[0].deck = p1Deck
-	state.players[1].deck = p2Deck
-
-	state.players[0].hand = p1Hand
-	state.players[1].hand = p2Hand
-
-	return state
-}
+export const setupAttackersDraw = (state: IState) => drawFullHand(state)
 
 export const setupAttackersPlace = (state: IState, {order}: IArg) => {
 	state.players[0].hand = []
@@ -25,5 +15,3 @@ export const setupAttackersPlace = (state: IState, {order}: IArg) => {
 
 	return state
 }
-
-const splitArrayAt = <T>(i: number, arr: T[]) => [arr.slice(0, i), arr.slice(i)]
