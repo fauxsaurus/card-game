cce01a46db6f599e2dc1348b7265d513e40cc2da
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-11-15 15:06:06 -0600

    data type fix
    Attackers were always supposed to be an object with keys of 0-5, never an array.

--- src/lib/setup-attackers.ts
+++ src/lib/setup-attackers.ts
@@ -10,8 +10,8 @@ export const setupAttackersPlace = (state: IState, {order}: IArg) => {
 	state.players[0].hand = []
 	state.players[1].hand = []
 
-	state.players[0].attackers = order[0]
-	state.players[1].attackers = order[1]
+	state.players[0].attackers = Object.assign({}, order[0])
+	state.players[1].attackers = Object.assign({}, order[1])
 
 	return state
 }
